53e2820fc9e0645eddb2939ef4e493bee329b0fe
HangYongmao <1981217256@qq.com>
2018-02-14 17:55:04 +0800

    自定义位置的Toast显示

--- app/src/main/java/com/example/toastdemo/MainActivity.java
+++ app/src/main/java/com/example/toastdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.toastdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,18 +17,36 @@ public class MainActivity extends AppCompatActivity {
 
     // 初始化点击事件
     private void initEvent() {
+        // 显示默认的Toast
         findViewById(R.id.toast_btn1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showToast1();
             }
         });
+
+        // 显示自定义位置的Toast
+        findViewById(R.id.toast_btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast2();
+            }
+        });
+
+
     }
 
     // 显示默认Toast
     private void showToast1() {
 //        Toast toast = Toast.makeText(this, "默认Toast", Toast.LENGTH_SHORT);
         Toast toast = Toast.makeText(this, R.string.app_name, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    // 显示自定义位置的Toast
+    private void showToast2() {
+        Toast toast = Toast.makeText(this, "自定义位置的Toast", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
     }
 }
